ca4dcb09f043080240a5697ec016aa32ec48b470
Kieve Chua <kievechua@gmail.com>
2017-02-09 18:54:17 +0800

    Resolve

--- src/config/kue.js
+++ src/config/kue.js
@@ -1,7 +1,18 @@
 export default {
   watchStuckJobsInterval: 1000,
+  removeOnComplete: true,
+  shutdownTimeout: 5000,
+  ui: {
+    listen: 3000,
+    title: '',
+    express: {},
+    basicAuth: {
+      username: '',
+      password: ''
+    }
+  },
   redis: {
     host: 'localhost',
-    port: 6379,
-  },
-};
+    port: 6379
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,73 +1,86 @@
-import requireAll from 'require-all';
-import fs from 'mz/fs';
-import kue from 'kue';
-import Base from 'magnet-core/dist/base';
-import defaultConfig from './config/kue';
+// import requireAll from 'require-all'
+// import fs from 'mz/fs'
+import kue from 'kue'
+import Base from 'magnet-core/base'
+import defaultConfig from './config/kue'
 
 export default class MagnetKue extends Base {
-  async setup() {
-    this.kueConfig = Object.assign(defaultConfig, this.config.kue, this.options);
-    let queues = {};
-    try {
-      const folderPath = `${process.cwd()}/server/job_queues`;
-      await fs.exists(folderPath);
-      queues = requireAll(folderPath);
-    } catch (err) {
-      this.log.warn(err);
-    }
+  async setup () {
+    this.kueConfig = Object.assign(defaultConfig, this.config.kue, this.options)
 
-    this.app.queue = kue.createQueue(this.kueConfig);
+    this.app.kue = kue.createQueue(this.kueConfig)
 
     // https://github.com/Automattic/kue#error-handling
-    this.app.queue.on('error', (err) => {
-      this.log.error(err);
-    });
+    this.app.kue.on('error', (err) => {
+      this.log.error(err)
+    })
 
     // https://github.com/Automattic/kue#unstable-redis-connections
-    this.app.queue.watchStuckJobs(this.kueConfig.watchStuckJobsInterval);
+    this.app.kue.watchStuckJobs(this.kueConfig.watchStuckJobsInterval)
 
-    for (let key of Object.keys(queues)) {
-      let queue = queues[key].default || queues[key];
-      let name = queue.name || key;
-      let processArgs = [name];
-      if (queue.concurrency) {
-        processArgs.push(queue.concurrency);
-      }
-
-      if (queue.process) {
-        processArgs.push(async (data, ctx, done) => {
-          try {
-            done(null, await queue.process.call(this, this.app, data, ctx));
-          } catch (err) {
-            done(err);
+    if (this.kueConfig.removeOnComplete) {
+      this.app.kue
+      .on('job complete', (id, result) => {
+        kue.Job.get(id, (err, job) => {
+          if (err) {
+            this.log.error(err)
+            return
           }
-        });
-      } else {
-        this.log.warn(`No process for ${name}`);
-      }
-
-      this.app.queue.process.apply(this.app.queue, processArgs);
+          job.remove((err) => {
+            if (err) {
+              this.log.error(err)
+              return
+            }
+            this.log.info('removed completed job #%d', job.id)
+          })
+        })
+      })
     }
+
+    // let queues = {}
+    // try {
+    //   const folderPath = `${process.cwd()}/server/job_queues`
+    //   await fs.exists(folderPath)
+    //   queues = requireAll(folderPath)
+    // } catch (err) {
+    //   this.log.warn(err)
+    // }
+
+    // for (let key of Object.keys(queues)) {
+    //   let queue = queues[key].default || queues[key]
+    //   let name = queue.name || key
+    //   let processArgs = [name]
+    //   if (queue.concurrency) {
+    //     processArgs.push(queue.concurrency)
+    //   }
+    //
+    //   if (queue.process) {
+    //     processArgs.push(async (data, ctx, done) => {
+    //       try {
+    //         done(null, await queue.process.call(this, this.app, data, ctx))
+    //       } catch (err) {
+    //         done(err)
+    //       }
+    //     })
+    //   } else {
+    //     this.log.warn(`No process for ${name}`)
+    //   }
+    //
+    //   this.app.kue.process.apply(this.app.kue, processArgs)
+    // }
   }
 
-  async start() {
-    if (this.kueConfig.listen) {
-      kue.app.listen(this.kueConfig.listen);
-    }
-  }
-
-  async teardown() {
+  async teardown () {
     return new Promise((resolve, reject) => {
-      this.app.queue.shutdown(this.kueConfig.listen, (err, result) => {
-        this.app.log.error( 'Kue shutdown result: ', arguments);
-
+      this.app.kue.shutdown(this.kueConfig.shuwtdownTimeout, (err, result) => {
         if (err) {
-          reject(err);
-          return;
+          this.app.log.error(err)
+          reject(err)
+          return
         }
 
-        resolve(result);
-      });
-    });
+        resolve(result)
+      })
+    })
   }
 }
